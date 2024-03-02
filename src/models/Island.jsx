4fab4857d239b5c16f31d7c2caed8ab2410539dd
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-03-02 20:02:25 +0200

    feat: implemented island rotation

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -9,57 +9,180 @@ Title: Fox's islands
 import { useRef, useEffect } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
+import { a } from "@react-spring/three";
 
-const Island = (props) => {
-  const { nodes, materials } = useGLTF("/island.glb");
+import islandScene from "../assets/3d/island.glb";
+
+const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
+  const islandRef = useRef();
+
+  const { gl, viewport } = useThree();
+  const { nodes, materials } = useGLTF(islandScene);
+
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  /* ___mouse_event_functions___ */
+
+  const handlePointerDown = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    // Determine the horizontal position of the pointer (mouse or touch)
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+    // Update the last recorded X-coordinate for future calculations
+    lastX.current = clientX;
+  };
+
+  const handlePointerUp = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(false);
+  };
+
+  const handlePointerMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (isRotating) {
+      // Determine the horizontal position of the pointer (mouse or touch)
+      const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+      // Calculate the change in position normalized by the viewport width
+      const delta = (clientX - lastX.current) / viewport.width;
+
+      // Update the rotation of the object based on the calculated delta
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      // Update the last recorded X-coordinate for the next calculation
+      lastX.current = clientX;
+      // Update the rotation speed with the calculated delta
+      rotationSpeed.current = delta * 0.01 * Math.PI;
+    }
+  };
+
+  /* ___keyboard_event_functions___ */
+
+  const handleKeyDown = (e) => {
+    if (e.key === "ArrowLeft") {
+      if (!isRotating) {
+        setIsRotating(true);
+        // Rotate the object to the left by a small angle
+        islandRef.current.rotation.y += 0.01 * Math.PI;
+      } else if (e.key === "ArrowRight") {
+        if (!isRotating) {
+          setIsRotating(true);
+          // Rotate the object to the right by a small angle
+          islandRef.current.rotation.y -= 0.01 * Math.PI;
+        }
+      }
+    }
+  };
+
+  const handleKeyUp = (e) => {
+    if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+      setIsRotating(false);
+    }
+  };
+
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.01) {
+        rotationSpeed.current = 0;
+      }
+      islandRef.current.rotation.y += rotationSpeed.current;
+    } else {
+      const rotation = islandRef.current.rotation.y;
+      /**
+       * Normalize the rotation value to ensure it stays within the range [0, 2 * Math.PI].
+       * The goal is to ensure that the rotation value remains within a specific range to
+       * prevent potential issues with very large or negative rotation values.
+       *  Here's a step-by-step explanation of what this code does:
+       *  1. rotation % (2 * Math.PI) calculates the remainder of the rotation value when divided
+       *     by 2 * Math.PI. This essentially wraps the rotation value around once it reaches a
+       *     full circle (360 degrees) so that it stays within the range of 0 to 2 * Math.PI.
+       *  2. (rotation % (2 * Math.PI)) + 2 * Math.PI adds 2 * Math.PI to the result from step 1.
+       *     This is done to ensure that the value remains positive and within the range of
+       *     0 to 2 * Math.PI even if it was negative after the modulo operation in step 1.
+       *  3. Finally, ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI) applies another
+       *     modulo operation to the value obtained in step 2. This step guarantees that the value
+       *     always stays within the range of 0 to 2 * Math.PI, which is equivalent to a full
+       *     circle in radians.
+       */
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  });
+
+  useEffect(() => {
+    const canvas = gl.domElement;
+    canvas.addEventListener("pointerdown", handlePointerDown);
+    canvas.addEventListener("pointerup", handlePointerUp);
+    canvas.addEventListener("pointermove", handlePointerMove);
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      canvas.removeEventListener("pointerdown", handlePointerDown);
+      canvas.removeEventListener("pointerup", handlePointerUp);
+      canvas.removeEventListener("pointermove", handlePointerMove);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keyup", handleKeyUp);
+    };
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove]);
+
   return (
-    <group {...props} dispose={null}>
+    <a.group ref={islandRef} {...props}>
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface944_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface945_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface946_tree2_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface947_tree1_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface948_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.polySurface949_tree_body_0.geometry}
         material={materials.PaletteMaterial001}
       />
       <mesh
-        castShadow
-        receiveShadow
         geometry={nodes.pCube11_rocks1_0.geometry}
         material={materials.PaletteMaterial001}
       />
-    </group>
+    </a.group>
   );
-}
-
-useGLTF.preload("/island.glb");
+};
 
 export default Island;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
-import { Suspense } from "react";
+import { useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader";
 import { SpotLight } from "@react-three/drei";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
 
 {
   /*<div className="absolute top-28 left-0 right-0 z-20 flex items-center justify-center">
@@ -10,18 +14,60 @@ import { SpotLight } from "@react-three/drei";
 }
 
 const Home = () => {
+  const [currentStage, setCurrentStage] = useState(1);
+  const [isRotating, setIsRotating] = useState(false);
+
+  const adjustIslanForScreenSize = () => {
+    let screenScale = window.innerWidth < 768 ? [0.9, 0.9, 0.9] : [1, 1, 1];
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    return [screenScale, screenPosition, rotation];
+  };
+
+  const adjustPlaneForScreenSize = () => {
+    let screenScale = window.innerWidth < 768 ? [1.5, 1.5, 1.5] : [3, 3, 3];
+    let screenPosition = window.innerWidth < 768 ? [0, -1.5, 0] : [0, -4, -4];
+
+    return [screenScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition, rotation] = adjustIslanForScreenSize();
+  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+
   return (
     <section className="w-full h-screen relative">
       <Canvas
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#FAA300"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Bird />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={rotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+          <Plane
+            isRotating={isRotating}
+            position={planePosition}
+            scale={planeScale}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>

3ec44a2e529cfd6975dff05f60346d8857896427
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-03-10 23:34:38 +0200

    feat: added header to the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,30 @@ import React from "react";
 
 // About me component page with information about me, my technology stack
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="max-container">
+      <h1 className="head-text">
+        Hello, I'm{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Bohdan
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Fullstack Web Developer in Ukraine, specializing in creating dynamic
+          and user-friendly web applications using HTML, CSS, JavaScript,
+          React.js, Three.js, Tailwind CSS. Goal is to deliver high-quality,
+          responsive, and scalable solutions to meet client needs.
+        </p>
+      </div>
+      <div className="py-10 flex flex-colw">
+        <h3 className="subhead-text">My Skills</h3>
+        <div className="mt-16 flex flex-wrap">
+          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;

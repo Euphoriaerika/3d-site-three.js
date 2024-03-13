c2a5612954f3a468674afd12c0399bd62abe1b5c
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-03-13 17:29:37 +0200

    feat: implemented Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,8 +1,71 @@
+import { Link } from "react-router-dom";
+
+import CTA from "../components/CTA";
+
+import { arrow } from "../assets/icons";
+import { projects } from "../constants";
+
 // Portfolio page component
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section className="max-container">
+      <h1 className="head-text">
+        My{" "}
+        <span className="blue-gradient_text font-semibold drop-shadow">
+          Projects
+        </span>
+      </h1>
+      <div className="mt-5 flex flex-col gap-3 text-slate-500">
+        <p>
+          Throughout the past year, I've executed numerous projects, and some of
+          them are particularly dear to my heart, so I can't help but share them
+          with you. All of these projects have open-source code, meaning you can
+          not only review them but also use them in your own projects.
+          Additionally, I'm always open to your additions and recommendations,
+          so feel free to share your ideas with me!
+        </p>
+      </div>
+      <div className="flex flex-wrap my-20 gap-16">
+        {projects.map((project) => (
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
+              <div className={`btn-back rounded-xl ${project.theme}`} />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img
+                  src={project.iconUrl}
+                  alt="Project Icon"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
+                {project.name}
+              </h4>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
+                <Link
+                  to={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-blue-600"
+                >
+                  Live Link
+                </Link>
+                <img
+                  src={arrow}
+                  alt="arrow"
+                  className="w-4 h-4 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr className="border-slate-200" />
+      <CTA />
+    </section>
+  );
+};
 
-export default Projects
+export default Projects;
